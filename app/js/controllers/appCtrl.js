6eea0b73510e326315f292fb71fd854364aae65e
Roland <rskalmogo@gmail.com>
2016-06-08 06:34:06 -0400

    refactoring appCtrl

--- app/js/controllers/appCtrl.js
+++ app/js/controllers/appCtrl.js
@@ -1,22 +1,29 @@
-angular.module('app')
+(function(){
+    'use strict';
+    
+    angular.module('app')
        .controller('appCtrl',appCtrl);
 
-function appCtrl(dataFactory,$scope){
-    
-    dataFactory.updateData();
-     $scope.search=function () {
-        dataFactory.updateData($scope.query);
+    function appCtrl(dataFactory,$scope){
+        $scope.query={};
+        dataFactory.updateData();
+        
+        $scope.search=function () {
+            console.log("searching for "+$scope.query.q);
+            dataFactory.updateData($scope.query);
+        }
+        
+        $scope.reset=function(){
+            $scope.query.q="";
+            dataFactory.updateData($scope.query);
+        };
+        
+        $scope.$watch(function () { return dataFactory.getData(); }, function (newValue, oldValue) {
+            if (newValue !== oldValue) $scope.data = newValue;
+        });
+        
     }
+
+})();
     
-    $scope.reset=function(){
-        $scope.query.q="";
-        dataFactory.updateData($scope.query);
-    };
-    
-    $scope.$watch(function () { return dataFactory.getData(); }, function (newValue, oldValue) {
-        if (newValue !== oldValue) $scope.data = newValue;
-    });
-    
-    
-}
         
