79f85097469755307912f7e9877b2b1f00d1ee56
Roland <rskalmogo@gmail.com>
2016-06-08 00:57:41 -0400

    no result fix

--- app/js/controllers/appCtrl.js
+++ app/js/controllers/appCtrl.js
@@ -8,6 +8,11 @@ function appCtrl(dataFactory,$scope){
         dataFactory.updateData($scope.query);
     }
     
+    $scope.reset=function(){
+        $scope.query.q="";
+        dataFactory.updateData($scope.query);
+    };
+    
     $scope.$watch(function () { return dataFactory.getData(); }, function (newValue, oldValue) {
         if (newValue !== oldValue) $scope.data = newValue;
     });
